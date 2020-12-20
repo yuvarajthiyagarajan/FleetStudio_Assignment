08fc5307cc6cb7690f40d2fd0fd38704cf1a148b
Yuvaraj Thiyagarajan <tyuvaraj16@gmail.com>
2020-12-20 11:57:25 +0530

    Implemented session timeout based on json configuration property file

--- App/__init__.py
+++ App/__init__.py
@@ -3,12 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from datetime import timedelta
-
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+
+with open('App/app_config.json', 'r') as f:
+	data = json.load(f)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=data['session_timeout_interval'])
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- App/routes.py
+++ App/routes.py
@@ -10,13 +10,13 @@ from App.password_validator import CheckPassword
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Yuvaraj',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Santhosh',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
